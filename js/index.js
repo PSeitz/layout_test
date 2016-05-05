a72feef5de4eed1db936a891c3e82ff6c14a60cb
Pascal Seitz <pascal.seitz@sap.com>
2016-05-05 08:47:58 +0200

    vanilla js version

--- js/index.js
+++ js/index.js
@@ -5,25 +5,6 @@
  * Add specific delay for CSS3-animation to elements.
  */
 
-// (function($) {
-//     var speed = 2000;
-//     var container =  $('.display-animation');
-//     container.each(function() {
-//         var elements = $(this).children();
-//         elements.each(function() {
-//             var elementOffset = $(this).offset();
-//             var offset = elementOffset.left*0.8 + elementOffset.top;
-//             var delay = parseFloat(offset/speed).toFixed(2);
-//             $(this)
-//                 .css("-webkit-animation-delay", delay+'s')
-//                 .css("-o-animation-delay", delay+'s')
-//                 .css("animation-delay", delay+'s')
-//                 .addClass('animated');
-//         });
-//     });
-// })(jQuery);
-
-
 (function() {
     var speed = 2000;
     var elements = document.querySelectorAll('.display-animation');
@@ -46,49 +27,7 @@
 
 })();
 
-/**
- * Created by Kupletsky Sergey on 04.09.14.
- *
- * Ripple-effect animation
- * Tested and working in: ?IE9+, Chrome (Mobile + Desktop), ?Safari, ?Opera, ?Firefox.
- * JQuery plugin add .ink span in element with class .ripple-effect
- * Animation work on CSS3 by add/remove class .animate to .ink span
-*/
-
-(function($) {
-    $(".ripple-effect").click(function(e){
-        var rippler = $(this);
-
-        // create .ink element if it doesn't exist
-        if(rippler.find(".ink").length == 0) {
-            rippler.append("<span class='ink'></span>");
-        }
-
-        var ink = rippler.find(".ink");
-
-        // prevent quick double clicks
-        ink.removeClass("animate");
-
-        // set .ink diametr
-        if(!ink.height() && !ink.width())
-        {
-            var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-            ink.css({height: d, width: d});
-        }
-
-        // get click coordinates
-        var x = e.pageX - rippler.offset().left - ink.width()/2;
-        var y = e.pageY - rippler.offset().top - ink.height()/2;
-
-        // set .ink position and add class .animate
-        ink.css({
-            top: y+'px',
-            left:x+'px'
-        }).addClass("animate");
-    })
-})(jQuery);
-
-(function($) {
+(function() {
 
     var elements = document.querySelectorAll('.ripple-effect');
     Array.prototype.forEach.call(elements, function(rippler, i){
@@ -96,8 +35,9 @@
         rippler.addEventListener('click', function (e) {
             // create .ink element if it doesn't exist
 
-            if(rippler.querySelector(".ink") !== null) {
-                parent.appendChild("<span class='ink'></span>");
+            if(rippler.querySelector(".ink") == null) {
+                var span = rippler.appendChild(document.createElement('span'));
+                span.classList.add('ink');
             }
 
             var ink = rippler.querySelector(".ink");
@@ -106,16 +46,34 @@
             ink.classList.remove("animate");
 
             // set .ink diametr
-            if(!ink.height() && !ink.width())
+            if(!ink.offsetHeight && !ink.offsetWidth)
             {
-                var d = Math.max(rippler.outerWidth(), rippler.outerHeight());
-                ink.css({height: d, width: d});
+                var d = Math.max(rippler.offsetWidth,  outerHeight(rippler));
+                ink.style["height"] = d+'px';
+                ink.style["width"] = d+'px';
             }
 
+            // get click coordinates
+            var offsetTop = rippler.getBoundingClientRect().top + document.body.scrollTop;
+            var offsetLeft = rippler.getBoundingClientRect().left + document.body.scrollLeft;
+            var x = e.pageX - offsetLeft - ink.offsetWidth/2;
+            var y = e.pageY - offsetTop - ink.offsetHeight/2;
+
+            ink.style["top"] = y+'px';
+            ink.style["left"] = x+'px';
+            ink.classList.add('animate');
             
         });
 
     });
 
-})(jQuery);
+})();
 
+
+function outerHeight(el) {
+  var height = el.offsetHeight;
+  var style = getComputedStyle(el);
+
+  height += parseInt(style.marginTop) + parseInt(style.marginBottom);
+  return height;
+}
